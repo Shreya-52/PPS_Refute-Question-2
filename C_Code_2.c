78edc628818a64534d3fb9484a3af1b6a0ef7171
Shreya-52 <ss8382@srmist.edu.in>
2024-12-09 14:31:18 +0530

    Add files via upload

---
+++ C_Code_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int find_largest(int a, int b, int c) {
+    if (a > b && a > c)
+        return a;
+    else if (b > c)
+        return b;
+    else
+        return c;
+}
+
---
+++ Python_Code_2.py
@@ -0,0 +1,8 @@
+def find_largest(a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > c:
+        return b
+    else:
+        return c
+
